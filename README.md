567cc47986a458a3b5fcdac0020d9b6c91f40731
Wongsakorn Khamwong <wongsakorn_kha@truecorp.co.th>
2018-07-08 21:15:47 +0700

    -edit readme file - add api document -

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# todo
+# TODO (Task) API
+## Requirement
+- MySQL
+- Node.js
+
+## Install 
+pull repository
+
+```
+git pull https://github.com/besthalo/todo.git
+```
+
+go to project Directory
+```curl
+cd ./{project_name}
+```
+
+install package , use npm or yarn 
+
+```curl
+npm install
+```
+
+start service
+
+```curl
+node server.js
+```
+
+
+test service:
+[Health Check](http://localhost:3000/v1/healthcheck)
---
+++ extra_file/work.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Jul 08, 2018 at 04:43 AM
+-- Server version: 10.3.4-MariaDB-10.3.4+maria~jessie
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `work`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `detail` text NOT NULL,
+  `status` enum('done','pending') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status` (`status`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
